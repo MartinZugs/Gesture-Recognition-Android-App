01105a31df4bde25b6a711f80f96a21e344d72f0
Jack Kotheimer.edu <jkotheimer@luc.edu>
2019-05-14 13:28:04 -0500

    added Beeper class to create a beeper object which can be toggled to play

---
+++ app/src/main/java/com/example/gesturerecognition/Beeper.java
@@ -0,0 +1,40 @@
+package com.example.gesturerecognition;
+
+import android.app.Activity;
+import android.net.rtp.AudioStream;
+
+import android.media.MediaPlayer;
+
+import com.example.gesturerecognition.R;
+
+
+public class Beeper {
+
+    private static AudioStream audioStream;
+
+    private static MediaPlayer mediaPlayer;
+
+    // current status of clip
+    boolean status = false;
+
+    //allows usage of getApplicationContext()
+    private Activity activity;
+
+    public Beeper(Activity a) throws Exception {
+        activity = a;
+        mediaPlayer = MediaPlayer.create(activity.getBaseContext(), R.raw.beep_sound);
+    }
+
+
+    public void toggle() throws Exception{
+        if(status) {
+            mediaPlayer.stop();
+            mediaPlayer.prepare();
+            status = false;
+        } else {
+            mediaPlayer.start();
+            mediaPlayer.setLooping(true);
+            status = true;
+        }
+    }
+}
