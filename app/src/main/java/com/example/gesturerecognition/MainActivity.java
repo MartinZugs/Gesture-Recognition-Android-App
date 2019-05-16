d63d6c8af8e6512f55432cb2aceef2b09972419d
Martin Zugschwert <martin.e.zugschwert@gmail.com>
2019-05-16 11:11:17 -0500

    Name changed to offState

--- app/src/main/java/com/example/gesturerecognition/MainActivity.java
+++ app/src/main/java/com/example/gesturerecognition/MainActivity.java
@@ -125,13 +125,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     public void checkMotion (final float ax, final float ay, final float az)
     {
-        if ((ax > 2.5 || ax < -2.5) && !(sm.getBeeper().isPlaying()) && ((ax > ay) && (ax > az) || (ax < ay) && (ax <az))) {
+        if ((ax > 3 || ax < -3) && !(sm.getBeeper().isPlaying()) && ((ax > ay) && (ax > az) || (ax < ay) && (ax <az))) {
             sm.x_move();
         }
-        else if ((az > 2.5 || az < -2.5) && !(sm.getBeeper().isPlaying()) && ((az > ay) && (az > ax) || (az < ay) && (az <ax))) {
+        else if ((az > 3 || az < -3) && !(sm.getBeeper().isPlaying()) && ((az > ay) && (az > ax) || (az < ay) && (az <ax))) {
             sm.z_move();
         }
-        else if ((ay > 2.5 || ay < -2.5) && !(sm.getBeeper().isPlaying()) && ((ay > ax) && (ay > az) || (ay < ax) && (ay < az))) {
+        else if ((ay > 3 || ay < -3) && !(sm.getBeeper().isPlaying()) && ((ay > ax) && (ay > az) || (ay < ax) && (ay < az))) {
             sm.y_move();
         }
     }
