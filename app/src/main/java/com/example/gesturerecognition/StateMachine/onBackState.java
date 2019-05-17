d8d7eff9b6a9b9e89f73819175362fc880c9c30e
Martin Zugschwert <martin.e.zugschwert@gmail.com>
2019-05-17 11:54:10 -0500

    Got rid of extra text fields

--- app/src/main/java/com/example/gesturerecognition/StateMachine/onBackState.java
+++ app/src/main/java/com/example/gesturerecognition/StateMachine/onBackState.java
@@ -1,35 +1,23 @@
 package com.example.gesturerecognition.StateMachine;
 
-import android.widget.TextView;
-import com.example.gesturerecognition.R;
-import org.w3c.dom.Text;
-
 public class onBackState implements State {
 
     private StateMachine sm;
 
-    private TextView t;
-
-    public onBackState(final StateMachine sm) {
-        this.sm = sm;
-        t = sm.getActivity().getLastMotion();
-    }
+    public onBackState(final StateMachine sm) { this.sm = sm; }
 
     @Override
     public void x_move() {
-        t.setText("I don't care!");
         sm.getBeeper().saySomething("I don't care");
     }
 
     @Override
     public void y_move() {
-        t.setText("Here you go.");
         sm.getBeeper().saySomething("Here you go");
     }
 
     @Override
     public void z_move() {
-        t.setText("I don't know");
         sm.getBeeper().saySomething("I don't know");
     }
 }
